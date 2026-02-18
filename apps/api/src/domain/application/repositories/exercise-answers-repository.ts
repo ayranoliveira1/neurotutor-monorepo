498a46bca7ea5866b8cc7a501dd436f265e29fbb
ayranoliveira1 <ayranoliveira1@gmail.com>
2026-02-18 18:48:36 -0300

    feat(api): add ExerciseListsRepository and ExerciseAnswersRepository interfaces

---
+++ apps/api/src/domain/application/repositories/exercise-answers-repository.ts
@@ -0,0 +1,12 @@
+import { ExerciseAnswer } from '@/domain/entreprise/entities/exercise-answer'
+
+export abstract class ExerciseAnswersRepository {
+  abstract createOrUpdate(answer: ExerciseAnswer): Promise<void>
+  abstract findByListAndQuestion(
+    exerciseListId: string,
+    questionId: string,
+  ): Promise<ExerciseAnswer | null>
+  abstract findManyByListId(exerciseListId: string): Promise<ExerciseAnswer[]>
+  abstract countByListId(exerciseListId: string): Promise<number>
+  abstract saveManyIsCorrect(answers: ExerciseAnswer[]): Promise<void>
+}
